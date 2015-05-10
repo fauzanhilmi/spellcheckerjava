8b04c68edd39a91074593f66fcba2ed0b3b683c2
unknown <fauzan_hilmi@student.itb.ac.id>
2015-05-10 13:55:22 +0700

    dictionary udah

--- src/spellchecker/SpellChecker.java
+++ src/spellchecker/SpellChecker.java
@@ -5,17 +5,37 @@
  */
 package spellchecker;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author Fauzan Hilmi
  */
 public class SpellChecker {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static String filename = "dictionary.txt";
+    public static ArrayList<String> dict;
+    
+    public static void main(String[] args) throws IOException {
+        dict = new ArrayList<>();
+        Scanner fileScanner = new Scanner(new File(filename));
+        while(fileScanner.hasNextLine()) {
+            dict.add(fileScanner.nextLine());
+        }
+        String sl = "";
+        for(String s: dict) {
+            if(s.length()>sl.length()) sl = s;
+            //System.out.println(s);
+        }
+        System.out.println(sl);
+        System.out.println(sl.length());
     }
     
 }
